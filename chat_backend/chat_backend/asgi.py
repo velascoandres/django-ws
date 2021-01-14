79846b7fd7ec0a60fa3a0edf4566b32108802931
andres <velasco.andrs@gmail.com>
2021-01-13 22:41:25 -0500

    agregar canales

--- chat_backend/chat_backend/asgi.py
+++ chat_backend/chat_backend/asgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_backend.settings')
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import apps.chat.routing
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_backend.settings")
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            apps.chat.routing.websocket_urlpatterns
+        )
+    ),
+})
--- chat_backend/chat_backend/settings.py
+++ chat_backend/chat_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'apps.chat'
 ]
 
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+ASGI_APPLICATION = 'chat_backend.asgi.application'
